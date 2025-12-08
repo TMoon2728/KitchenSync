040390c0e33b87e11c533fc8ce2dd1e40b5c328c
TMoon2728 <78892764+TMoon2728@users.noreply.github.com>
2025-12-08 12:39:23 -0600

    fix: use regex for express 5 catch-all

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../dist')));
 
     // Handle React Routing (SPA catch-all)
-    app.get('*', (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.join(__dirname, '../dist', 'index.html'));
     });
 }
